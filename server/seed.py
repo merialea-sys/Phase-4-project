4d1c798f767dd8048af128f849d265b2faac5cae
merialea <merialea5@gmail.com>
2026-01-16 14:18:39 +0300

    update changes

--- server/seed.py
+++ server/seed.py
@@ -148,6 +148,21 @@ def seed_data():
 
         print("Database seeding complete!")
 
+        #admin user
+        admin = User(
+            username="admin", 
+            email="admin@bank.com", 
+            first_name="Admin", 
+            last_name="User", 
+            is_admin=True
+        )
+
+        admin.password_hash = "admin123"
+
+        db.session.add(admin)
+        db.session.commit()
+        exit()
+
 
 if __name__ == "__main__":
     seed_data()
